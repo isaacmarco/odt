718ee3b36d4d32a0eec1860f8a6bebf304de315a
Isaac Marco <isaacmarco@gmail.com>
2019-06-18 21:07:00 +0100

    nuevas tools

--- procesamiento odt (copia).js
+++ procesamiento odt (copia).js
@@ -6,7 +6,7 @@
 </head>
 
 <style>
-#holder { border: 10px dashed #ccc; width: 300px; height: 100px; margin: 20px auto;}
+#holder { border: 10px dashed #ccc; width: 300px; height: 300px; margin: 20px auto;}
 #holder.hover { border: 10px dashed #333; }
 </style>
 
@@ -97,51 +97,8 @@ holder.ondrop = function (e) {
   this.className = '';
   e.preventDefault();
   
-	var fichero = e.dataTransfer.files[0]; 		
-	handleFile(fichero);
+		//var file = e.dataTransfer.files[0]; 		
 	
-	
-	function handleFile(f) {
-				
-		 
-		JSZip.loadAsync(f)
-		.then(function(zip) {			
-				
-			zip.file("content.xml").async("string").then(function (data) {
-				
-				// formatear el XML 
-				var xmlFormateado = data; 					
-				xmlFormateado = vkbeautify.xml(xmlFormateado);
-					
-					
-				// el vkbeautify identa el codigo pero introduce en la cabecera
-				// del XML saltos de linea dentro de las cadenas de texto, hay 
-				// que utilizar la cabecera linealizada
-					
-				// linearizamos el XML 
-				//var xmlLinearizado = vkbeautify.xmlmin(xmlFormateado);					
-				// expresion regular para encontrar la cabcera del XML 
-				var cabeceraXMLdefecto = '<office:document-content xmlns:office="urn:oasis:names:tc:opendocument:xmlns:office:1.0" xmlns:style="urn:oasis:names:tc:opendocument:xmlns:style:1.0" xmlns:text="urn:oasis:names:tc:opendocument:xmlns:text:1.0" xmlns:table="urn:oasis:names:tc:opendocument:xmlns:table:1.0" xmlns:draw="urn:oasis:names:tc:opendocument:xmlns:drawing:1.0" xmlns:fo="urn:oasis:names:tc:opendocument:xmlns:xsl-fo-compatible:1.0" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:meta="urn:oasis:names:tc:opendocument:xmlns:meta:1.0" xmlns:number="urn:oasis:names:tc:opendocument:xmlns:datastyle:1.0" xmlns:svg="urn:oasis:names:tc:opendocument:xmlns:svg-compatible:1.0" xmlns:chart="urn:oasis:names:tc:opendocument:xmlns:chart:1.0" xmlns:dr3d="urn:oasis:names:tc:opendocument:xmlns:dr3d:1.0" xmlns:math="http://www.w3.org/1998/Math/MathML" xmlns:form="urn:oasis:names:tc:opendocument:xmlns:form:1.0" xmlns:script="urn:oasis:names:tc:opendocument:xmlns:script:1.0" xmlns:ooo="http://openoffice.org/2004/office" xmlns:ooow="http://openoffice.org/2004/writer" xmlns:oooc="http://openoffice.org/2004/calc" xmlns:dom="http://www.w3.org/2001/xml-events" xmlns:xforms="http://www.w3.org/2002/xforms" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:rpt="http://openoffice.org/2005/report" xmlns:of="urn:oasis:names:tc:opendocument:xmlns:of:1.2" xmlns:xhtml="http://www.w3.org/1999/xhtml" xmlns:grddl="http://www.w3.org/2003/g/data-view#" xmlns:tableooo="http://openoffice.org/2009/table" xmlns:textooo="http://openoffice.org/2013/office" xmlns:field="urn:openoffice:names:experimental:ooo-ms-interop:xmlns:field:1.0" office:version="1.2"><office:scripts/>';
-				var cadena = '<office:document-content.*<office:scripts\/>';
-				var expresion = new RegExp(cadena, 's');
-				// obtenemos la cabecera correcta linearizada
-				//var cabecera = xmlLinearizado.match(expresion);
-				// sustituimos la cabecera lineaziada por la antigua
-				xmlFormateado = xmlFormateado.replace(expresion, cabeceraXMLdefecto);
-					
-				// linearizar el xml y mostrarlo en el text area 
-				var xmlLinearizado = vkbeautify.xmlmin(xmlFormateado);
-					
-				// mostrar en los text areas
-				$('#codigoXML').val(xmlFormateado);
-				$('#codigoXMLLinearizado').val(xmlLinearizado);			
-				
-				
-			});			
-			
-		}); 		
-		
-    }	
 
   return false;
 }; 
@@ -153,7 +110,12 @@ holder.ondrop = function (e) {
 var $result = $("#result");
 
 $("#file").on("change", function(evt) {
-    
+    // remove content
+    $result.html("");
+    // be sure to show the results
+    $("#result_block").removeClass("hidden").addClass("show");
+
+    // Closure to capture the file information.
     function handleFile(f) {
 				
 		 
@@ -190,17 +152,82 @@ $("#file").on("change", function(evt) {
 				$('#codigoXMLLinearizado').val(xmlLinearizado);			
 				
 				
-			});			
+			});
 			
-		}); 		
+			
+		}); 
+				
+			
 		
-    }	
+
+		/*
+      
+        JSZip.loadAsync(f)                                   // 1) read the Blob
+        .then(function(zip) {
+				
+
+            zip.forEach(function (relativePath, zipEntry) {  // 2) print entries
+				
+				if( zipEntry.name == 'content.xml'){
+					
+									
+					// el contenido de content.xml queda en la variable data 
+					zip.file("content.xml").async("string").then(function (data) {
+					
+					// formatear el XML 
+					var xmlFormateado = data; 					
+					xmlFormateado = vkbeautify.xml(xmlFormateado);
+					
+					
+					// el vkbeautify identa el codigo pero introduce en la cabecera
+					// del XML saltos de linea dentro de las cadenas de texto, hay 
+					// que utilizar la cabecera linealizada
+					
+					// linearizamos el XML 
+					//var xmlLinearizado = vkbeautify.xmlmin(xmlFormateado);					
+					// expresion regular para encontrar la cabcera del XML 
+					var cabeceraXMLdefecto = '<office:document-content xmlns:office="urn:oasis:names:tc:opendocument:xmlns:office:1.0" xmlns:style="urn:oasis:names:tc:opendocument:xmlns:style:1.0" xmlns:text="urn:oasis:names:tc:opendocument:xmlns:text:1.0" xmlns:table="urn:oasis:names:tc:opendocument:xmlns:table:1.0" xmlns:draw="urn:oasis:names:tc:opendocument:xmlns:drawing:1.0" xmlns:fo="urn:oasis:names:tc:opendocument:xmlns:xsl-fo-compatible:1.0" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:meta="urn:oasis:names:tc:opendocument:xmlns:meta:1.0" xmlns:number="urn:oasis:names:tc:opendocument:xmlns:datastyle:1.0" xmlns:svg="urn:oasis:names:tc:opendocument:xmlns:svg-compatible:1.0" xmlns:chart="urn:oasis:names:tc:opendocument:xmlns:chart:1.0" xmlns:dr3d="urn:oasis:names:tc:opendocument:xmlns:dr3d:1.0" xmlns:math="http://www.w3.org/1998/Math/MathML" xmlns:form="urn:oasis:names:tc:opendocument:xmlns:form:1.0" xmlns:script="urn:oasis:names:tc:opendocument:xmlns:script:1.0" xmlns:ooo="http://openoffice.org/2004/office" xmlns:ooow="http://openoffice.org/2004/writer" xmlns:oooc="http://openoffice.org/2004/calc" xmlns:dom="http://www.w3.org/2001/xml-events" xmlns:xforms="http://www.w3.org/2002/xforms" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:rpt="http://openoffice.org/2005/report" xmlns:of="urn:oasis:names:tc:opendocument:xmlns:of:1.2" xmlns:xhtml="http://www.w3.org/1999/xhtml" xmlns:grddl="http://www.w3.org/2003/g/data-view#" xmlns:tableooo="http://openoffice.org/2009/table" xmlns:textooo="http://openoffice.org/2013/office" xmlns:field="urn:openoffice:names:experimental:ooo-ms-interop:xmlns:field:1.0" office:version="1.2"><office:scripts/>';
+					var cadena = '<office:document-content.*<office:scripts\/>';
+					var expresion = new RegExp(cadena, 's');
+					// obtenemos la cabecera correcta linearizada
+					//var cabecera = xmlLinearizado.match(expresion);
+					// sustituimos la cabecera lineaziada por la antigua
+					xmlFormateado = xmlFormateado.replace(expresion, cabeceraXMLdefecto);
+					
+					// linearizar el xml y mostrarlo en el text area 
+					var xmlLinearizado = vkbeautify.xmlmin(xmlFormateado);
+					
+					// mostrar en los text areas
+					$('#codigoXML').val(xmlFormateado);
+					//$('#codigoXMLLinearizado').val(xmlLinearizado);				
+					
+					
+					});
+				
+				}
+				
+				
+            });
+			
+			
+        }, function (e) {
+			 
+            //$result.append($("<div>", {
+            //    "class" : "alert alert-danger",
+            //    text : "Error reading " + f.name + ": " + e.message
+            //}));
+			 
+        });
+		*/
+		
+    }
 	
-	 
+	
+
     var files = evt.target.files;
     for (var i = 0; i < files.length; i++) {
         handleFile(files[i]);
-    } 
+    }
 	
 });
 
